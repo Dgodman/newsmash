8e48889af0211eadb91af172fb8890851f27cba1
lemonhead <dgodman@gmail.com>
2017-09-11 20:09:40 -0400

    init

---
+++ masher/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MasherConfig(AppConfig):
+    name = 'masher'
---
+++ masher/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def index(request):
+    '''Home page of site'''
+
+    # render html
+    return render(
+        request,
+        'masher/index.html',
+        {},
+    )
